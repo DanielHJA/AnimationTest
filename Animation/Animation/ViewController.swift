8ac56a5f41f14042bddc26f181bef6d5e7b8ada2
Daniel <daniel.hjartstrom@gmail.com>
2017-07-21 22:57:53 +0200

    safe copy

--- Animation/Animation/ViewController.swift
+++ Animation/Animation/ViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var constant: CGFloat = 8.0
+    var constant: CGFloat = 10.0
     var screenWidth: CGFloat = 0.0
     var screenHeight: CGFloat = 0.0
     var colorView: UIView?
+    var layer: CAShapeLayer = CAShapeLayer()
+    var path: UIBezierPath = UIBezierPath()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,17 +26,34 @@ class ViewController: UIViewController {
         colorView = UIView(frame: CGRect(x: 0, y: 0, width: screenWidth / constant, height: screenWidth / constant))
         
         colorView?.backgroundColor = UIColor.green
-        
-        colorView?.layer.borderWidth = 0.5
-        colorView?.layer.borderColor = UIColor.black.cgColor
-        
+
         self.view.addSubview(colorView!)
         
-        
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
-        
+    
+        layer = CAShapeLayer()
+        layer.strokeColor = UIColor.green.cgColor
+        layer.lineWidth = 60
+        layer.fillColor = UIColor.clear.cgColor
+
+        var path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: self.view.frame.maxX, y: 0))
+        path.addLine(to: CGPoint(x: self.view.frame.maxX, y: self.view.frame.maxY))
+        path.addLine(to: CGPoint(x: 0, y: self.view.frame.maxY))
+        path.close()
+            
+        layer.path = path.cgPath
+        var animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.fromValue = Float(0.0)
+        animation.toValue = Float(1.0)
+        animation.duration = 20.0
+        layer.add(animation, forKey: "myStroke")
+
+        view.layer.addSublayer(layer)
+
         animate()
         
     }
@@ -45,8 +64,8 @@ class ViewController: UIViewController {
         
     }
     
-    var rounds: Int = 1
-    let totalDuration = 12.0
+    var rounds: Int = 0
+    let totalDuration = 20.0
 
     func animate(){
         
@@ -57,32 +76,32 @@ class ViewController: UIViewController {
             
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: self.seconds(second: 3), animations: {
                 
-                self.colorView?.frame.origin.x += self.view.frame.maxX - (square * iterations)
+                self.colorView?.frame.origin.x = self.view.frame.maxX - square - (square * iterations)
                 
             })
             
-            UIView.addKeyframe(withRelativeStartTime: self.seconds(second: 3), relativeDuration: self.seconds(second: 3), animations: {
+            UIView.addKeyframe(withRelativeStartTime: self.seconds(second: 3), relativeDuration: self.seconds(second: 7), animations: {
                 
-                self.colorView?.frame.origin.y += self.view.frame.maxY - (square * iterations)
+                self.colorView?.frame.origin.y = self.view.frame.maxY - square - (square * iterations)
                 
             })
             
-            UIView.addKeyframe(withRelativeStartTime: self.seconds(second: 6), relativeDuration: self.seconds(second: 3), animations: {
+            UIView.addKeyframe(withRelativeStartTime: self.seconds(second: 10), relativeDuration: self.seconds(second: 3), animations: {
                 
-                self.colorView?.frame.origin.x -= self.view.frame.maxX - (square * iterations)
+                self.colorView?.frame.origin.x = 0 + (square * iterations)
                 
             })
            
-            UIView.addKeyframe(withRelativeStartTime: self.seconds(second: 9), relativeDuration: self.seconds(second: 3), animations: {
+            UIView.addKeyframe(withRelativeStartTime: self.seconds(second: 13), relativeDuration: self.seconds(second: 7), animations: {
                 
-                self.colorView?.frame.origin.y -= self.view.frame.maxY - (square * iterations)
+                self.colorView?.frame.origin.y = 0 + (square * iterations)
                 
             })
        
         }, completion: { (completion) in
     
-            if self.rounds == 3 {
-                
+            if self.rounds == 8 {
+                print("done")
                 return
            
             } else {
